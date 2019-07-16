3988763b76a34fbf560750d167e5c4fc56c87fcf
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 11:37:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,11 +53,7 @@ class Student
   end
   
   def self.students_below_12th_grade 
-    sql = <<-SQL 
-      SELECT *
-      FROM students
-      WHERE grade < 12
-    SQL
+    sql = "SELECT * FROM students WHERE grade < 12"
   
     DB[:conn].execute(sql)
   end
