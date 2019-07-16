e6ae52abb0417a506e32d2946989992925cb2bea
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 11:28:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
   
   def self.first_X_students_in_grade_10(number_students)
     sql = <<-SQL
-    SELECT *
+      SELECT *
       FROM students 
       WHERE grade = "10" 
       LIMIT number_students
@@ -72,7 +72,18 @@ class Student
     
     DB[:conn].execute(sql)
   end
-      
+  
+  def self.first_student_in_grade_10 
+    sql <<-SQL 
+      SELECT *
+      FROM students 
+      WHERE grade = "10"
+      LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
