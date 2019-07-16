1d3268cab298a4af1a6b7bd8188e2b90bc799648
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 11:22:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
       WHERE grade <= "11"
     SQL
   
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql)
   end
   
   def self.first_X_students_in_grade_10(number_students)
