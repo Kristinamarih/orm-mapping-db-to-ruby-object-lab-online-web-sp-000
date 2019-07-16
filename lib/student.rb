7f1761a2d957711c025f93db26aed1ad773c4544
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-16 11:43:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,8 @@ class Student
     sql = "SELECT * FROM students WHERE grade < 12"
   
     DB[:conn].execute(sql).map do |row|
-      
+      self.new_from_db(row)
+    end
   end
   
   def self.first_X_students_in_grade_10(x)
